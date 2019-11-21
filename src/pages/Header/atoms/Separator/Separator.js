a1efd2edfa8512c181ee2dd1f1b52bc938b4dce1
Harsh Gupta <hgupta@tekion.com>
2019-11-21 10:54:57 +0530

    added a header Component and left header Component

---
+++ src/pages/Header/atoms/Separator/Separator.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Icon from '../../../../atoms/Icons';
+const LeftHeader = (props) => {
+
+  return (
+    <div>
+      <Icon url = {props.url} />
+    </div>
+  );
+}
+
+export default LeftHeader;
